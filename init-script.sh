e92e3435944b1f40af2ba3e7293d2416df6a6fcc
Chris Slamar <chris@slamar.com>
2013-03-22 14:55:38 -0500

    Init script framwork and renamed main deamon

---
+++ init-script.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# chkconfig: 35 90 12
+# description: gelfPush daemon
+#
+# Get function from functions library
+. /etc/init.d/functions
+
+# Source networking configuration.
+. /etc/sysconfig/network
+
+RETVAL=0
+
+prog="gelfPush"
+thelock=/var/log/subsys/gelfPush
+
+# Start the service FOO
+start() {
+	[ -f /etc/gelfPush.conf ] || exit 6
+	echo -n $"Starting $prog: "
+	if [ $UID -ne -0 ]; then
+		RETVAL=1
+		failure
+	else
+		daemon /usr/local/sbin/gelfPush
+		RETVAL=$?
+		[ $RETVAL -eq 0 ] && touch $thelock
+	fi;
+	echo
+	return $RETVAL
+}
+# Restart the service FOO
+stop() {
+	echo -n $"Stopping $prog: "
+	if [ UID -ne 0 ]; then
+		RETVAL=1
+		failure
+	else
+		killproc gelfPush
+		RETVAL=$?
+		[ $RETVAL -eq 0 ] && rm -f $thelock
+	fi;
+	echo
+	return $RETVAL
+}
+
+reload(){
+	echo -n $"Reloading $prog: "
+	killproc gelfPush -HUP
+	RETVAL=$?
+	echo
+	return $RETVAL
+}
+
+restart(){
+	stop
+	start
+}
+### main logic ###
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	status)
+		status gelfPush
+		$RETVAL=$?
+		;;
+	reload)
+		reload
+		;;
+	restart)
+		stop
+		start
+		;;
+	*)
+		echo $"Usage: $0 {start|stop|restart|reload|status}"
+		exit 1
+esac
+exit $RETVAL
