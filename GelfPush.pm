3b82cc5ff4f4c130c22eaebc65697836cef3c39c
Chris Slamar <chris@slamar.com>
2013-03-23 15:05:52 -0500

    Removed extra lines added debugging

--- GelfPush.pm
+++ GelfPush.pm
@@ -18,11 +18,12 @@ $VERSION		= 0.0.1;
 %EXPORT_TAGS	= ( DEFAULT => [qw(&sendToGraylog &watcher_secure)]);
 
 sub sendToGraylog {
-	### ARGS: level, facility, file, short, long, hostname
+	### ARGS: level, facility, file, short, long, hostname, debug
 	my $datetime = `date +%s`;
 	chomp($datetime);
 	my $hostname = `hostname`;
 	chomp($hostname);
+	my $debug = $_[6];
 
 	my $gelf_format = { 
 	        "version" => "1.0",
@@ -37,7 +38,7 @@ sub sendToGraylog {
 
 	my $json_doc = encode_json($gelf_format);
 
-	print "\n$json_doc\n\n";
+	if($debug) { print "\n$json_doc\n\n"; }
 
 	my $gzipped_message;
 	gzip \$json_doc => \$gzipped_message or die "gzip failed! $GzipError\n\n";
@@ -48,12 +49,13 @@ sub sendToGraylog {
 		Proto => 'udp',
 	) or die "Error in socket creation : $!\n";
 
-	print "UDP Connection sucessful!!!\n";
-
-	print "Sending Message...\n";
+	if( $debug ) {
+		print "UDP Connection sucessful!!!\n";
+		print "Sending Message...\n";
+	}
 	$socket->send($gzipped_message);
 	$socket->close();
-	print "Message Sent!\n";
+	if($debug) { print "Message Sent!\n"; }
 }
 
 sub watcher_apache_access {
@@ -77,19 +79,17 @@ sub watcher_apache_access {
 	while( defined( my $line = $file->read ) ) {
 		chomp($line);
 		(my $request, my $statusCode) = $line =~ m/\S+ - \S+ - \S+ \S+ \[.*\] \"(.*)\" (\d+) \S+ \".*\" \".*\"/;
-		# $rule =~ m/(\S+) - (\S+) - (\S+) (\S+) (\[.*\]) (\".*\") (\d+) (\S+) (\".*\") (\".*\")/;
-		# print "$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9\n$10\n";
 		print "Request: $request\n";
 		print "Status Code: $statusCode\n";
 		(my $short) = $request =~ m/(^.{1,30})/;
 		if( $statusCode =~ m/401|403|500|502|503/ ) {
 			print "Error!!! $statusCode returned!\n";
 			print "Short: $short\n";
-			sendToGraylog( 3, 'Apache Access', '/var/log/httpd/access_log', $short, $line, $hostname );
+			sendToGraylog( 3, 'Apache Access', '/var/log/httpd/access_log', $short, $line, $hostname, $debug );
 		} else {
 			print "Ok!  $statusCode returned!\n";
 			print "Short: $short\n";
-			sendToGraylog( 6, 'Apache Access', '/var/log/httpd/access_log', $short, $line, $hostname );
+			sendToGraylog( 6, 'Apache Access', '/var/log/httpd/access_log', $short, $line, $hostname, $debug );
 		}
 	}
 }
@@ -111,34 +111,26 @@ sub watcher_secure {
 	while( defined( my $line = $file->read ) ) {
 		chomp($line);
 		my $rule = $line;
-#		print "Rule: $rule\n";
-		if( $rule =~ m/sudo/ ) {
-#			print "$rule\n";
-			(my $who, my $command) = $rule =~ m/sudo: (\S+) \:.* (COMMAND\=.+)/;
+			if( $rule =~ m/sudo/ ) {
+				(my $who, my $command) = $rule =~ m/sudo: (\S+) \:.* (COMMAND\=.+)/;
 			print "SUDO: $who $command\n";
 			my $short = "SUDO: $who $command";
 			(my $full) = $rule =~ m/(sudo: .*)/;
-#			print "Full: $full\n";
-			sendToGraylog( 4, 'Secure Log', '/var/log/secure', $short, $full, $hostname );
-				# level, facility, file, short, long, hostname
+			sendToGraylog( 4, 'Secure Log', '/var/log/secure', $short, $full, $hostname, $debug );
 		} elsif ( $rule =~ m/sshd/ && $rule !~ m/authentication failure/) {
 			if ( $rule =~ m/Accepted/ && $rule !~ m/cacti/ ) {
 				(my $who, my $from) = $rule =~ m/sshd.+ for (\S+) from (\S+)/;
-#				print "SSH: $who from $from\n";
-				my $short =  "SSH: $who from $from";
+					my $short =  "SSH: $who from $from";
 				(my $tmpFull) = $rule =~ m/sshd\[\d+\]\: (.*)/;
 				my $full = "SSH: " . $tmpFull;
-#				print "SSHFULL $full\n";
-				sendToGraylog( 4, 'Secure Log', '/var/log/secure', $short, $full, $hostname );
+				sendToGraylog( 4, 'Secure Log', '/var/log/secure', $short, $full, $hostname, $debug );
 			}
 		} elsif ( $rule =~ m/pam_sss\(sshd\:auth\)\: authentication failure/ ) {
 			(my $from, my $who) = $rule =~ m/rhost\=(\S+) user\=(\S+)/;
-#			print "SSH FAILURE: $who from $from\n";
-			my $short = "SSH FAILURE: $who from $from";
+				my $short = "SSH FAILURE: $who from $from";
 			(my $tmpFull) = $rule =~ m/sshd\[\d+\]\: (.*)/;
 			my $full = "SSH: " . $tmpFull;
-#			print "SSHFULL $full\n";
-			sendToGraylog( 2, 'Secure Log', '/var/log/secure', $short, $full, $hostname );
+			sendToGraylog( 2, 'Secure Log', '/var/log/secure', $short, $full, $hostname, $debug );
 		}
 	}
 }
